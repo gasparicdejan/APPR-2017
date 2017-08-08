eee36004783592323f7f16307c8d526dea97e4d3
Dejan <deyangasparic@gmail.com>
2017-08-08 15:56:43 +0200

    dopolnil in popravil napake

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,4 +1,6 @@
-# 3. faza: Vizualizacija podatkov
+# 3. faza: Vizualizacija podatkov in
+# 4. faza: Analiza podatkov
+
 
 library(knitr)
 library(ggplot2)
@@ -152,64 +154,52 @@ print(zem1)
 
 
 
-# Zemljevid pretvorimo in ker ima v pretvorjeni obliki za določene države druga imena, kot so 
-# v podatkih, za vsako državo, ki ima drugo ime prilagodimo tako, da se imeni ujemata.
-
-sv2 <- pretvori.zemljevid(svet)
-
-sv2$name_long <- as.character(sv1$name_long)
-sv2$name_long[sv2$name_long == "Anguilla"] <- "Anguila"
-sv2$name_long[sv2$name_long == "Bahamas"] <- "Bahamas, The"
-sv2$name_long[sv2$name_long == "Republic of COngo"] <- "Congo, Rep."
-sv2$name_long[sv2$name_long == "Democratic Republic of the Congo"] <- "Congo, Dem. Rep."
-sv2$name_long[sv2$name_long == "Ethiopia"] <- "Ethiopia(excludes Eritrea)"
-sv2$name_long[sv2$name_long == "Egypt"] <- "Egypt, Arab Rep."
-sv2$name_long[sv2$name_long == "Falkland Islands"] <- "Falkland Island"
-sv2$name_long[sv2$name_long == "The Gambia"] <- "Gambia, The"
-sv2$name_long[sv2$name_long == "Hong Kong"] <- "Hong Kong, China"
-sv2$name_long[sv2$name_long == "Heard I. and McDonald Islands"] <- "Heard Island and McDonald Isla"
-sv2$name_long[sv2$name_long == "Iran"] <- "Iran, Islamic Rep."
-sv2$name_long[sv2$name_long == "Republic of Korea"] <- "Korea, Rep."
-sv2$name_long[sv2$name_long == "Kyrgyzstan"] <- "Kyrgyz Republic"
-sv2$name_long[sv2$name_long == "Macedonia"] <- "Macedonia, FYR"
-sv2$name_long[sv2$name_long == "Netherlands"] <- "Netherlands Antilles"
-sv2$name_long[sv2$name_long == "Pitcairn Islands"] <- "Pitcairn"
-sv2$name_long[sv2$name_long == "Saint Maarten"] <- "Saint Maarten (Dutch part)"
-sv2$name_long[sv2$name_long == "Sao Tomé and Principe"] <- "Sao Tome and Principe"
-sv2$name_long[sv2$name_long == "Serbia"] <- "Serbia, FR(Serbia/Montenegro)"
-sv2$name_long[sv2$name_long == "Turks and Caicos Islands"] <- "Turks and Caicos Isl."
-sv2$name_long[sv2$name_long == "Wallis and Futura Islands"] <- "Wallis and Futura Isl."
-sv2$name_long[sv2$name_long == "French Southern and Antarctic Lands"] <- "Fr. So. Ant. Tr"
-sv2$name_long[sv2$name_long == "Brunei Durassalam"] <- "Brunei"
-sv2$name_long[sv2$name_long == "British Indian Ocean Territory"] <- "British Indian Ocean Ter."
-sv2$name_long[sv2$name_long == "Saint Kitts and Nevis"] <- "St. Kitts and Nevis"
-sv2$name_long[sv2$name_long == "Dem.Rep.Korea"] <- "Korea, Dem. Rep."
-sv2$name_long[sv2$name_long == "Saint Lucia"] <- "St. Lucia"
-sv2$name_long[sv2$name_long == "Côte d'Ivoire"] <- "Cote d'Ivoire"
-sv2$name_long[sv2$name_long == "American Samoa"] <- "Samoa"
-sv2$name_long[sv2$name_long == "Slovakia"] <- "Slovak Republic"
-sv2$name_long[sv2$name_long == "Syria"] <- "Syrian Arab Republic"
-sv2$name_long[sv2$name_long == "South Sudan"] <- "Fm Sudan"
-sv2$name_long[sv2$name_long == "Timor-Leste"] <- "East Timor"
-sv2$name_long[sv2$name_long == "Federated States of Micronesia"] <- "Micronesia, Fed. Sts."
-sv2$name_long[sv2$name_long == "Cook Islands"] <- "Cocos (Keeling) Islands"
-sv2$name_long[sv2$name_long == "Saint Vincent and the Grenadines"] <- "St. Vincent and the Grenadines"
-
 
 # Tabela "Uvoz" bo predstavljalo tabelo podatkov za leto 2015. 
 Uvoz <- filter(tabela_uvoz, Leto == 2015)
 Uvoz$Partner <- factor(Uvoz$Partner)
 
 # Nastavimo tako, da se v tabeli sv1, stolpec name_long ujema z stolpcem Partner v tabeli Izvoz:
-sv2$name_long<- factor(sv2$name_long, levels = levels(Uvoz$Partner))
+sv1$name_long<- factor(sv1$name_long, levels = levels(Uvoz$Partner))
 
 # NArišemo zemljevid za uvoz ZDA:
 zem2 <- ggplot() +
   geom_polygon(data = Uvoz %>%
-                 right_join(sv2, by = c("Partner" = "name_long")),
+                 right_join(sv1, by = c("Partner" = "name_long")),
                aes(x=long, y=lat, group = group, fill = Vsi_produkti),
                color = "grey") + ggtitle("Celotni uvoz ZDA iz držav po svetu v letu 2015")+
   scale_fill_gradient(low = "Ghostwhite", high = "brown3", guide = "colourbar")+ xlab("")+
   ylab("")
 
 print(zem2)
+
+
+#################### TRGOVINSKA BILANCA: ####################
+
+# Sestavljena tabela po Partnerjih in letu za izvoz in uvoz:
+uvoz_izvoz_tabela <- inner_join(tabela_uvoz, tabela_izvoz, by= c("Partner", "Leto"))
+
+
+# Tabela "trg_bilanca" bo prikazovala trgovinsko bilanco med ZDA in državami za vsako leto:
+trg_bilanca <- data.frame(uvoz_izvoz_tabela$Partner, uvoz_izvoz_tabela$Leto, 
+                          uvoz_izvoz_tabela$Vsi_produkti.y - uvoz_izvoz_tabela$Vsi_produkti.x)
+names(trg_bilanca) <- c("Partner", "Leto", "Trgovinska_bilanca")
+
+
+# Zemljevid "zem3" bo prikazal trgovinsko bilanco med ZDA in državami za leto 2015:
+
+Bilanca <- filter(trg_bilanca, Leto == 2015)
+Bilanca$Partner <- factor(Bilanca$Partner)
+
+sv1$name_long <- factor(sv1$name_long, levels = levels(Bilanca$Partner))
+
+zem3 <- ggplot() +
+  geom_polygon(data = Bilanca %>%
+                 right_join(sv1, by = c("Partner" = "name_long")),
+               aes(x=long, y=lat, group = group, fill = Trgovinska_bilanca),
+               color = "grey") + 
+  ggtitle("Trgovinska bilanca med ZDA in državami po svetu v letu 2015")+
+  scale_fill_gradient(low = "Ghostwhite", high = "blue", guide = "colourbar")+ xlab("")+
+  ylab("")
+
+print(zem3)
